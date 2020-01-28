38c2e16dfa8ae4dd2d89c80d70a217a3037e65cd
Samykills <ullas.gupta91@gmail.com>
2020-01-28 17:56:32 +0100

    adding lottie

--- app/components/splash.js
+++ app/components/splash.js
@@ -1,8 +1,17 @@
 import React from 'react';
 import {View, Text} from 'react-native';
+import LottieView from 'lottie-react-native';
 
 const Splash = () => (
   <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+    <LottieView
+      loop={true}
+      autoPlay={true}
+      cacheStrategy={'strong'}
+      source={require('../resources/animations/groove_walk.json')}
+      hardwareAccelerationAndroid={true}
+      style={{width: 200, height: 200}}
+    />
     <Text style={{fontSize: 24, fontWeight: 'bold'}}>Stepgether</Text>
   </View>
 );
