9f895aa61175408bfc2e398589400db2a2f20c26
Samykills <ullas.gupta91@gmail.com>
2020-04-15 22:31:19 +0200

    refactor bit of code

--- app/components/common/chip.js
+++ app/components/common/chip.js
@@ -1,25 +1,14 @@
 import React from 'react';
-import {TouchableHighlight, Text} from 'react-native';
+import {TouchableHighlight, Text, StyleSheet} from 'react-native';
 import COLORS from '../../theme/colors';
 import PropTypes from 'prop-types';
-
 const Chip = ({value, onPress}) => {
   const onPressChip = () => {
     onPress(value);
   };
   return (
-    <TouchableHighlight
-      style={{
-        padding: 5,
-        borderRadius: 30,
-        borderWidth: 1,
-        borderColor: COLORS.WHITE,
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: COLORS.BLUE,
-      }}
-      onPress={onPressChip}>
-      <Text style={{color: COLORS.WHITE, fontWeight: 'bold'}}>{value}</Text>
+    <TouchableHighlight style={[styles.touchContainer]} onPress={onPressChip}>
+      <Text style={[styles.textStyle]}>{value}</Text>
     </TouchableHighlight>
   );
 };
@@ -28,4 +17,17 @@ Chip.proptypes = {
   value: PropTypes.string,
   onPress: PropTypes.func,
 };
+
+const styles = StyleSheet.create({
+  touchContainer: {
+    padding: 5,
+    borderRadius: 30,
+    borderWidth: 1,
+    borderColor: COLORS.WHITE,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: COLORS.BLUE,
+  },
+  textStyle: {color: COLORS.WHITE, fontWeight: 'bold'},
+});
 export default Chip;
